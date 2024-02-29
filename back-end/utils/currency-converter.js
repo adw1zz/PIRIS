176934bcf48a6202cea8babd8e43109b8456fc0c
Ivan Eliseev <vanae089@gmail.com>
2024-02-29 18:06:14 +0300

    parser

---
+++ back-end/utils/currency-converter.js
@@ -0,0 +1,13 @@
+module.exports = class CurrencyConverter {
+    static stringToNumber(str = "0.000,00") {
+        const numberString = str.replace(/\./g, '').replace(',', '.');
+        return parseFloat(numberString);
+    }
+
+    static numberToString(num = 0.0) {
+        const numberString = num.toFixed(2);
+        const parts = numberString.split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        return parts.join(",");
+    }
+}
